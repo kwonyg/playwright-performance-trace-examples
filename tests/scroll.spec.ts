4af4359a8437ace964c37ebf769e0b34f2a8a82c
kwonyg <andamirocll@gmail.com>
2022-06-01 17:58:09 +0900

    행위 주석 추가

--- tests/scroll.spec.ts
+++ tests/scroll.spec.ts
@@ -8,12 +8,15 @@ test('scroll performance test', async ({ browser, page }) => {
   await page.goto('https://m.search.naver.com/search.naver?where=m_view&sm=mtb_jum&query=bts');
 
   await browser.startTracing(page, { path: 'scroll-trace.json' });
+
+  // 특정 요소까지 scroll
   for (let i = 0; i < 10; i++) {
     await page.$eval('.u_pg_btn', (e) => {
       e.scrollIntoView({ behavior: 'smooth', block: 'end', inline: 'end' });
     });
     await page.waitForTimeout(2000);
   }
+
   await browser.stopTracing();
 });
 ``;
